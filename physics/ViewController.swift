d1b8af6dc9239c12a274d17cee27c1f970e9577b
Valery Girkin <vary10@gmail.com>
2015-03-17 16:52:31 +0300

    Initial Commit

---
+++ physics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  physics
+//
+//  Created by Valery Girkin on 17.03.15.
+//  Copyright (c) 2015 Valery Girkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
